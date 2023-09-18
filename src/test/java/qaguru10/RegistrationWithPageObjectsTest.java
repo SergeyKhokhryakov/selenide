b72283f0a6b48ee7b25f34a361aea494af865f2b
SergeyKhokhryakov <hso@list.ru>
2023-09-18 13:24:45 +0300

    Added comment

--- src/test/java/qaguru10/RegistrationWithPageObjectsTest.java
+++ src/test/java/qaguru10/RegistrationWithPageObjectsTest.java
@@ -14,6 +14,9 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
+/**
+ * Using PageObjects
+ */
 
 public class RegistrationWithPageObjectsTest extends TestBase {
 
